59b76213b4f4d35eaff1c45794f40db45f7eda58
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-28 11:48:34 -0400

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-function validateForm(){
+$(document).ready(function validateForm(){
+	
 	/* Escribe tú código aquí */
-}
+		var nombre = $("#name").val();
+		var apellido = $("#lastname").val();
+		var correo = $("#input-email").val();
+		var password = $("input-password").val();
+
+		if(!(/[A-Za-z\s]/.test(nombre))){
+			alert("Ingrese su nombre");
+		}
+		if(!(/[A-Za-z\s]/.test(apellido))){
+			alert("Ingrese su apellido");
+		}
+		if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo)) ) {
+  			alert("Ingrese un correo válido");
+		}
+		if((password.length < 6) || (password == 123456) || (password == 098765)){
+			alert("Ingrese una clave válida");
+		}
+	
+
+})
+	
